ecffd46c0d2c204d17a6a309ccb427170a1a40af
Tianxiao Jiang <jtxinnocence@gmail.com>
2015-01-31 17:04:10 -0600

    Use ffmpeg for video audio merge
    Signed-off-by: Tianxiao Jiang <jtxinnocence@gmail.com>

--- src/methods/video_audio_merge.m
+++ src/methods/video_audio_merge.m
@@ -1,15 +1,17 @@
 function video_audio_merge(outfile,vfile,afile)
 %video_audio_merge.m
 
-%outfile: The merged output file name 
+%outfile: The merged output file name
 %vfile: Video file name
 %afile: Audio file name
 
 %mmread and mmwrite library is required!
-video=mmread(vfile);
-[tmp,audio]=mmread(afile);
-
-mmwrite(outfile,video,audio);
-
+% video=mmread(vfile);
+% [tmp,audio]=mmread(afile);
+% 
+% mmwrite('tmp.avi',video,audio);
+% 
+% ffmpegtranscode('tmp.avi',outfile,'VideoCodec','mpeg4');
+!ffmpeg -i vfile -i afile -map 0:v -map 1:a outfile
 end
 
