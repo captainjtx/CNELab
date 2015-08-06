ab8d5925d9d9f506ed28986714084a90e1459e9a
Tianxiao Jiang <jtxinnocence@gmail.com>
2015-08-06 12:33:31 -0500

    Add SpatialMapWindow

--- src/class/TFMapWindow.m
+++ src/class/TFMapWindow.m
@@ -26,6 +26,11 @@ classdef TFMapWindow < handle
         min_freq_edit
         max_clim_edit
         min_clim_edit
+        max_freq_slider
+        min_freq_slider
+        max_clim_slider
+        min_clim_slider
+        onset_radio
         
         method
         data_input
@@ -46,7 +51,7 @@ classdef TFMapWindow < handle
         end
         function buildfig(obj)
             obj.fig=figure('MenuBar','none','Name','Time-Frequency Map','units','pixels',...
-                'Position',[500 100 300 500],'NumberTitle','off','CloseRequestFcn',@(src,evts) OnClose(obj),...
+                'Position',[500 100 300 600],'NumberTitle','off','CloseRequestFcn',@(src,evts) OnClose(obj),...
                 'Resize','off');
             
             hp=uipanel('units','normalized','Position',[0,0,1,1]);
@@ -57,7 +62,7 @@ classdef TFMapWindow < handle
             obj.method_popup=uicontrol('Parent',hp_method,'Style','popup',...
                 'String',{'Channel','Average','Grid'},'units','normalized','Position',[0.4,0,0.59,0.92]);
             
-            hp_data=uipanel('Parent',hp,'Title','','Units','normalized','Position',[0,0.73,1,0.2]);
+            hp_data=uipanel('Parent',hp,'Title','','Units','normalized','Position',[0,0.78,1,0.15]);
             uicontrol('Parent',hp_data,'Style','text','String','Input Data: ','units','normalized','Position',[0.01,0.6,0.4,0.35],...
                 'HorizontalAlignment','left');
             obj.data_popup=uicontrol('Parent',hp_data,'Style','popup',...
@@ -76,7 +81,7 @@ classdef TFMapWindow < handle
             obj.ms_after_edit=uicontrol('Parent',hp_data,'Style','Edit','string','','units','normalized','position',[0.7,0.05,0.29,0.3],...
                 'HorizontalAlignment','left','visible','off');
             
-            hp_mag=uipanel('Parent',hp,'Title','','units','normalized','position',[0,0.66,1,0.06]);
+            hp_mag=uipanel('Parent',hp,'Title','','units','normalized','position',[0,0.73,1,0.04]);
             uicontrol('Parent',hp_mag,'style','text','units','normalized','string','Unit: ','position',[0.01,0,0.3,1],...
                 'HorizontalAlignment','left');
             obj.unit_mag_radio=uicontrol('Parent',hp_mag,'Style','radiobutton','units','normalized','string','dB','position',[0.4,0,0.29,1],...
@@ -84,7 +89,7 @@ classdef TFMapWindow < handle
             obj.unit_db_radio=uicontrol('Parent',hp_mag,'Style','radiobutton','units','normalized','string','Mag','position',[0.7,0,0.29,1],...
                 'HorizontalAlignment','left','callback',@(src,evts) UnitRadioCallback(obj,src));
             
-            hp_scale=uipanel('Parent',hp,'Title','','units','normalized','position',[0,0.45,1,0.2]);
+            hp_scale=uipanel('Parent',hp,'Title','','units','normalized','position',[0,0.57,1,0.15]);
             uicontrol('Parent',hp_scale,'style','text','units','normalized','string','Normalization: ',...
                 'position',[0.01,0.6,0.4,0.35],'HorizontalAlignment','left');
             obj.normalization_popup=uicontrol('Parent',hp_scale,'style','popup','units','normalized',...
@@ -102,17 +107,17 @@ classdef TFMapWindow < handle
             obj.scale_end_edit=uicontrol('parent',hp_scale,'style','edit','units','normalized',...
                 'string','','position',[0.5,0.05,0.4,0.3],'HorizontalAlignment','left','visible','off');
             
-            hp_stft=uipanel('parent',hp,'title','','units','normalized','position',[0,0.34,1,0.1]);
+            hp_stft=uipanel('parent',hp,'title','','units','normalized','position',[0,0.46,1,0.1]);
             uicontrol('parent',hp_stft,'style','text','string','STFT Window (sample): ','units','normalized',...
-                'position',[0,0.6,0.5,0.4]);
+                'position',[0,0.6,0.5,0.3]);
             obj.stft_winlen_edit=uicontrol('parent',hp_stft,'style','edit','string','',...
                 'units','normalized','position',[0.05,0.1,0.4,0.46],'HorizontalAlignment','center');
             uicontrol('parent',hp_stft,'style','text','string','STFT Overlap (sample): ',...
-                'units','normalized','position',[0.5,0.6,0.5,0.4]);
+                'units','normalized','position',[0.5,0.6,0.5,0.3]);
             obj.stft_overlap_edit=uicontrol('parent',hp_stft,'style','edit','string','',...
                 'units','normalized','position',[0.55,0.1,0.4,0.46],'HorizontalAlignment','center');
             
-            hp_freq=uipanel('parent',hp,'title','Frequency','units','normalized','position',[0,0,0.35,0.33]);
+            hp_freq=uipanel('parent',hp,'title','Frequency','units','normalized','position',[0,0,0.35,0.45]);
             
             uicontrol('parent',hp_freq,'style','text','string','Min','units','normalized',...
                 'position',[0,0.8,0.5,0.2]);
@@ -120,20 +125,32 @@ classdef TFMapWindow < handle
                 'position',[0.5,0.8,0.5,0.2]);
             
             obj.min_freq_edit=uicontrol('parent',hp_freq,'style','edit','string','','units','normalized',...
-                'position',[0.05,0.75,0.4,0.15],'horizontalalignment','center');
+                'position',[0.05,0.8,0.4,0.1],'horizontalalignment','center');
+            obj.min_freq_slider=uicontrol('parent',hp_freq,'style','slider','units','normalized',...
+                'position',[0.15,0.05,0.2,0.7]);
             obj.max_freq_edit=uicontrol('parent',hp_freq,'style','edit','string','','units','normalized',...
-                'position',[0.55,0.75,0.4,0.15],'horizontalalignment','center');
+                'position',[0.55,0.8,0.4,0.1],'horizontalalignment','center');
+            obj.max_freq_slider=uicontrol('parent',hp_freq,'style','slider','units','normalized',...
+                'position',[0.65,0.05,0.2,0.7]);
             
-            hp_clim=uipanel('parent',hp,'title','Power Limit','units','normalized','position',[0.36,0,0.35,0.33]);
+            hp_clim=uipanel('parent',hp,'title','Power Limit','units','normalized','position',[0.36,0,0.35,0.45]);
             
             uicontrol('parent',hp_clim,'style','text','string','Min','units','normalized',...
                 'position',[0,0.8,0.5,0.2]);
             uicontrol('parent',hp_clim,'style','text','string','Max','units','normalized',...
                 'position',[0.5,0.8,0.5,0.2]);
             obj.min_clim_edit=uicontrol('parent',hp_clim,'style','edit','string','','units','normalized',...
-                'position',[0.05,0.75,0.4,0.15],'horizontalalignment','center');
+                'position',[0.05,0.8,0.4,0.1],'horizontalalignment','center');
+            obj.min_clim_slider=uicontrol('parent',hp_clim,'style','slider','units','normalized',...
+                'position',[0.15,0.05,0.2,0.7]);
             obj.max_clim_edit=uicontrol('parent',hp_clim,'style','edit','string','','units','normalized',...
-                'position',[0.55,0.75,0.4,0.15],'horizontalalignment','center');
+                'position',[0.55,0.8,0.4,0.1],'horizontalalignment','center');
+            obj.min_clim_slider=uicontrol('parent',hp_clim,'style','slider','units','normalized',...
+                'position',[0.65,0.05,0.2,0.7]);
+            
+            hp_display=uipanel('parent',hp,'title','Display','units','normalized','position',[0.72,0,0.28,0.45]);
+            obj.onset_radio=uicontrol('parent',hp_display,'style','radiobutton','string','onset','value',1,...
+                'units','normalized','position',[0.1,0.9,0.9,0.1]);
         end
         function OnClose(obj)
             h = obj.fig;
