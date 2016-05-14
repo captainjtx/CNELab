61a5d80f6f2ae016fda9836429d0327785bf0143
Tianxiao Jiang <jtxinnocence@gmail.com>
2016-05-14 13:03:28 -0500

    setup modified

--- setup.m
+++ setup.m
@@ -1,7 +1,11 @@
 function setup()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-% set up environment variable for the system
+java_version=version('-java');
+
+if ~strcmp(java_version(6:8),'1.7')
+    disp('Matlab Java version mismatch !');
+    return;
+end
+
 while 1
     olddir=which('cnelab');
     
@@ -30,35 +34,16 @@ addpath(genpath([pwd filesep 'script']),'-frozen');
 addpath(genpath([pwd filesep 'test']),'-frozen');
 addpath(genpath([pwd filesep 'demo']),'-frozen');
 
-%recompile the java class to local platform, usually do not need
-java_version=version('-java');
 
-if ~strcmp(java_version(6:8),'1.7')
-    disp('Auto recompile java classes ...');
-    try
-    !javac src/java/LabelListBoxRenderer.java 
-    !javac src/java/TButton.java
-    !javac src/java/checkboxtree/FileLoadTree.java
-    catch
-        disp('Auto recompile failed !');
-        return
-    end
-end
-%save java class path into static file
 spath = javaclasspath('-static');
 if ~any(strcmp(spath,pwd))
     javaaddpath(pwd);
-end
-
-if ~any(strcmp(spath,[pwd filesep 'test']))
-    javaaddpath([pwd filesep 'test']);
 end
 
 pref_dir=prefdir;
 fid = fopen(fullfile(pref_dir,'javaclasspath.txt'),'w');
 
 fprintf(fid,'%s\n',pwd);
-fprintf(fid,'%s\n',[pwd filesep 'test']);
 
 fclose(fid);
 
