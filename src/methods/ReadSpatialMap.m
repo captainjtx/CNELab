8ae79282de6071496044a26fd1798e4f83bba8e0
Tianxiao Jiang <jtxinnocence@gmail.com>
2016-06-02 13:26:20 -0500

    Add ReadSpatialMap from cnelab

--- src/methods/ReadSpatialMap.m
+++ src/methods/ReadSpatialMap.m
@@ -2,7 +2,7 @@ function sm = ReadSpatialMap( filename )
 %READSPATIALMAP Summary of this function goes here
 %   Detailed explanation goes here
 fileID = fopen(filename);
-C = textscan(fileID,'%s%f',...
+C = textscan(fileID,'%s%f%d',...
     'Delimiter',',','TreatAsEmpty',{'NA','na'},'CommentStyle','%');
 fclose(fileID);
 
@@ -12,6 +12,11 @@ if length(C)<2
 else
     sm.name=C{1};
     sm.val=C{2};
+    sm.sig=zeros(length(C{1}),1);
+    if length(C)>2
+       sm.sig=C{3};
+    end
+    
 end
 end
 
