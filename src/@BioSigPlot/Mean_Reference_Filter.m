f2621873009923c00ba804490c47e5042caa8c7b
cnelab <jtxinnocence@gmail.com>
2016-11-21 13:11:47 -0600

    Mean_Reference Bug fix

--- src/@BioSigPlot/Mean_Reference_Filter.m
+++ src/@BioSigPlot/Mean_Reference_Filter.m
@@ -5,7 +5,7 @@ omitMask=true;
 data=data*(eye(size(data,2))-1/size(data,2)*ones(size(data,2),size(data,2)));
 
 for i=1:size(data,2)
-    obj.PreprocData{dataset(i)}(sample,channel(i))=data(:,i);
+    obj.PreprocData{dataset(i)}(sample-obj.BufferStartSample+1,channel(i))=data(:,i);
 end
 
 obj.redrawChangeBlock('time');
