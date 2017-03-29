967b568262d732bcf39b191c6a39d67998d4f9ff
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-03-29 22:39:46 +0000

    generate_grid_120_position.m edited online with Bitbucket

--- script/bmi/generate_grid_120_position.m
+++ script/bmi/generate_grid_120_position.m
@@ -2,6 +2,6 @@ fid=fopen('grid_12_10.txt','w');
 for i=1:120
     row=ceil(i/12);
     col=i-(row-1)*12;
-    fprintf(fid,'%s,%f,%f,%f\n',['C' num2str(i)],(col-1)*4,(row-1)*4,0);
+    fprintf(fid,'%s,%f,%f,%f\r\n',['C' num2str(i)],(col-1)*4,(row-1)*4,0);
 end
 fclose(fid);
