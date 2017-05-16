0747e21697ffa9ae2cac85c2c59fd4cee35a3984
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-05-16 13:34:48 -0500

    Update ReadSpatialMap

--- src/methods/ReadSpatialMap.m
+++ src/methods/ReadSpatialMap.m
@@ -2,21 +2,16 @@ function sm = ReadSpatialMap( filename )
 %READSPATIALMAP Summary of this function goes here
 %   Detailed explanation goes here
 fileID = fopen(filename);
-C = textscan(fileID,'%s%f%d',...
+C = textscan(fileID,'%s%f%d%f%f%f',...
     'Delimiter',',','TreatAsEmpty',{'NA','na'},'CommentStyle','%');
 fclose(fileID);
 
-if length(C)<2
-    sm=[];
-    return
-else
-    sm.name=C{1};
-    sm.val=C{2};
-    sm.sig=zeros(length(C{1}),1);
-    if length(C)>2
-       sm.sig=C{3};
-    end
-    
+sm.name=C{1};
+sm.val=C{2};
+sm.sig=zeros(length(C{1}),1);
+if ~any(isempty(C{3}))
+    sm.sig=C{3};
 end
+
 end
 
