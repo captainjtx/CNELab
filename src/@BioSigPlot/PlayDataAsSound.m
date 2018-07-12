37811604e5d31c4b487a63f56948f44a51a10747
Tianxiao Jiang <tj@cerebras.net>
2018-07-12 14:57:51 -0500

    Add files via upload

--- src/@BioSigPlot/PlayDataAsSound.m
+++ src/@BioSigPlot/PlayDataAsSound.m
@@ -1,9 +1,40 @@
+% %Contritubted by Heet Kaku, hnkaku@uh.edu (01/21/2018)
+% function PlayDataAsSound(obj)
+% [data,chanNames,~,~,~,~,~,~]=get_selected_data(obj);
+% if length(chanNames)>1
+%     errdlg('Please select only 1 channel!')
+% end
+% fs=max(min(obj.SRate,384000),1000);
+% sound((data./max(abs(data))),fs)
+% end
+
 %Contritubted by Heet Kaku, hnkaku@uh.edu (01/21/2018)
 function PlayDataAsSound(obj)
-[data,chanNames,~,~,~,~,~,~]=get_selected_data(obj);
-if length(chanNames)>1
-    errdlg('Please select only 1 channel!')
+prompt = {'Enter Sampling Rate:','Enter Absolute threshold:'};
+title = 'Parameters';
+dims = [1 35];
+definput = {'0 for default Fs of data','0 for no thresholding'};
+answer = inputdlg(prompt,title,dims,definput);
+answer = cellfun(@str2num,answer);
+if answer(1) == 0
+    fs=max(min(obj.SRate,384000),1000);
+else
+    fs = answer(1);
 end
-fs=max(min(obj.SRate,384000),1000);
+[data,chanNames,~,~,~,~,~,~]=get_selected_data(obj);
+
+threshold = answer(2);
+if threshold < 0 %Check if innout threshold is absolute value.
+    errordlg('Please select absolute value for thresholding!')
+elseif threshold ~= 0
+    ind=find(abs(data)<threshold); %Find indices of data less than threshold
+    data(ind)=0; %Make all those points 0.
+    if isempty(find(data))
+        errordlg('Threshold too large!')
+    end
+end
+if length(chanNames)>1
+    errordlg('Please select only 1 channel!')
+end
 sound((data./max(abs(data))),fs)
 end
