494100ac2be281e2ede82e8cc1dd362c219ad601
heetkaku <35621575+heetkaku@users.noreply.github.com>
2018-07-17 15:27:22 -0500

    Update PlayDataAsSound.m
    Updated function as per the new menu options.
    Fixed bug where audio would still play after showing error dialogue box

--- src/@BioSigPlot/PlayDataAsSound.m
+++ src/@BioSigPlot/PlayDataAsSound.m
@@ -1,40 +1,26 @@
-% %Contritubted by Heet Kaku, hnkaku@uh.edu (01/21/2018)
-% function PlayDataAsSound(obj)
-% [data,chanNames,~,~,~,~,~,~]=get_selected_data(obj);
-% if length(chanNames)>1
-%     errdlg('Please select only 1 channel!')
-% end
-% fs=max(min(obj.SRate,384000),1000);
-% sound((data./max(abs(data))),fs)
-% end
-
-%Contritubted by Heet Kaku, hnkaku@uh.edu (01/21/2018)
+%Contritubted by Heet Kaku, hnkaku@uh.edu (07/17/2018)
 function PlayDataAsSound(obj)
-prompt = {'Enter Sampling Rate:','Enter Absolute threshold:'};
-title = 'Parameters';
-dims = [1 35];
-definput = {'0 for default Fs of data','0 for no thresholding'};
-answer = inputdlg(prompt,title,dims,definput);
-answer = cellfun(@str2num,answer);
-if answer(1) == 0
+if obj.AudioSRate == 0
     fs=max(min(obj.SRate,384000),1000);
 else
-    fs = answer(1);
+    fs = obj.AudioSRate;
 end
 [data,chanNames,~,~,~,~,~,~]=get_selected_data(obj);
 
-threshold = answer(2);
-if threshold < 0 %Check if innout threshold is absolute value.
+if obj.AudioThreshold < 0 %Check if input threshold is absolute value.
     errordlg('Please select absolute value for thresholding!')
-elseif threshold ~= 0
-    ind=find(abs(data)<threshold); %Find indices of data less than threshold
+    return;
+elseif obj.AudioThreshold ~= 0
+    ind=find(abs(data)<obj.AudioThreshold); %Find indices of data less than threshold
     data(ind)=0; %Make all those points 0.
     if isempty(find(data))
         errordlg('Threshold too large!')
+        return;
     end
 end
 if length(chanNames)>1
     errordlg('Please select only 1 channel!')
+    return;
 end
-sound((data./max(abs(data))),fs)
+sound((data./max(abs(data))),fs) %Play the audio
 end
